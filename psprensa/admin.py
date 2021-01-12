2b10897599f26de443f0dc5f0751e639060327a3
Dani Nazare <dany.nt.14@gmail.com>
2021-01-12 14:25:13 -0300

    Add files via upload

---
+++ psprensa/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+"""
+User: psprensa
+Password: psprensa123456
+"""
+
+from .models import TabelaBin
+
+admin.site.register(TabelaBin)
---
+++ psprensa/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PsprensaConfig(AppConfig):
+    name = 'psprensa'
